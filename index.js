0ae708225585294d40938d785418355afbde7270
stankiiest <66993903+stankiiest@users.noreply.github.com>
2024-02-07 20:42:17 +0800

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Watching Netflix","Listening to Spotify","Playing GTA VI"];
+const statusMessages = ["watching v thru their window"];
 
 
 let currentIndex = 0;
